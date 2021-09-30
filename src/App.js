4e01be1e6d5ad16b1139f3a1f1134973a2b35da0
takahashikazatake <hiroyuki.takahashi@sencorp.co.jp>
2021-10-01 00:12:44 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from './Firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//screens
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import SignInOrUp from './screens/SignInOrUp';
+import SignUp from './screens/SignUp';
+
+import Auth from './Auth';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/signin" component={SignInOrUp} />
+                    <Route exact path="/signup" component={SignUp} />
+                    {/* 以下認証のみ */}
+                    <Auth>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/profile" component={Profile} />
+                            <Route render={() => <p>not found.</p>} />
+                        </Switch>
+                    </Auth>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
